716cb6300f6ec57d1c8836349e00190d31445517
Eyram Ekpe <ekpeeyram@yahoo.com>
2020-08-17 21:32:40 +0000

    Add-ups

--- site/js/script.js
+++ site/js/script.js
@@ -1,28 +1,28 @@
-// function makeMultiplier(multiplier) {
-//     var myFunFunc = function (x) {
-//       return multiplier * x;
-//     };
+// // function makeMultiplier(multiplier) {
+// //     var myFunFunc = function (x) {
+// //       return multiplier * x;
+// //     };
     
-//     return myFunFunc;
-//   }
+// //     return myFunFunc;
+// //   }
   
-//   var operation = makeMultiplier(10);
-//   console.log(operation(10));
+// //   var operation = makeMultiplier(10);
+// //   console.log(operation(10));
 
-// function test() {
-//     console.log("Hello Coursera!");
+// // function test() {
+// //     console.log("Hello Coursera!");
+// // }
+
+// // test(); 
+
+
+// var literalCircle = {
+//   radius: 10,
+
+//   getArea: function () {
+//     console.log(this);
+//     return Math.PI * Math.pow(this.radius, 2);
 // }
+// };
 
-// test(); 
-
-
-var literalCircle = {
-  radius: 10,
-
-  getArea: function () {
-    console.log(this);
-    return Math.PI * Math.pow(this.radius, 2);
-}
-};
-
-console.log(literalCircle.getArea());
+// console.log(literalCircle.getArea());
